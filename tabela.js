abaccb58ffb2a945c91b36089d8b7ae81099bae9
Bruno Tetzner <brunoltetzner@gmail.com>
2021-08-16 18:41:26 -0300

    COnstrução da tabela

--- tabela.js
+++ tabela.js
@@ -1,18 +1,13 @@
-//Fazer uma tabela com linhas e colunas com valor n.
-//Criar arrays com loops
-//somar em um unico array
-
-
 
 function array(n){                      
     let array = []
     
 
     
-    for(let j = 1; j<=n; j++){
+    for(let j = 0; j<=n; j++){
         marray = []
 
-        for(let i = 1; i<=n; i++){
+        for(let i = 0; i<=n; i++){
              marray.push(j*i)
             
         }
